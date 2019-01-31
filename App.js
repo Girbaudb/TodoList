97f9afdc2341dc8b26e70eece1b6cac11e8eb6b7
Girbaud Barthels <girbaudbarthels@gmail.com>
2019-01-31 14:34:05 +0100

    Update

--- App.js
+++ App.js
@@ -6,15 +6,8 @@
  * @flow
  */
 
-import React, {
-    Component
-} from 'react';
-import {
-    Platform,
-    StyleSheet,
-    Text,
-    View
-} from 'react-native';
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text,View} from 'react-native';
 
 const instructions = Platform.select({
     ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,26 +15,15 @@ const instructions = Platform.select({
         'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component < Props > {
+export default class App extends Component{
     render() {
-        return ( <
-            View style = {
-                styles.container
-            } >
-            <
-            Text style = {
-                styles.welcome
-            } > Welcome to GitHubSwag YOLO < /Text> <
-            Text style = {
-                styles.instructions
-            } > To get started, edit App.js < /Text> <
-            Text style = {
-                styles.instructions
-            } > {
-                instructions
-            } < /Text> <
-            /View>
+        return (
+        
+            <View style = {styles.container}>
+                <Text style = {styles.welcome}> Welcome to GitHubSwag</Text>
+                <Text style = { styles.instructions}> To get started, edit App.js</Text> 
+                <Text style = {styles.instructions}> {instructions} </Text> 
+            </View>
         );
     }
 }
